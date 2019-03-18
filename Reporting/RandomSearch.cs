5e2daf2addfd5b81cfc98fc63a9c53e3c32f1f29
Anusha Ammaluru <anammalu@microsoft.com>
2019-03-18 12:52:36 +0530

    Added Repo files

---
+++ Reporting/RandomSearch.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+using NUnit.Framework;
+
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+using Reporting.Properties;
+
+namespace Reporting
+{
+    [TestFixture]
+    internal partial class RandomSearch
+    {
+        private IWebDriver driver;
+
+        [SetUp]
+        public void SetUp()
+        {
+            this.driver = new ChromeDriver();
+            this.driver.Manage().Window.Maximize();
+            this.driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(20);
+        }
+
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.Category("Search Engine")]
+        [NUnit.Framework.Category("bvt")]
+        [NUnit.Framework.Author("anammalu")]
+        public void GoogleSearch()
+        {
+            this.driver.Navigate().GoToUrl(Resources.Google);
+            this.driver.FindElement(By.XPath(Resources.Query)).SendKeys("Extent Reports");
+            this.driver.FindElement(By.XPath(Resources.Query)).SendKeys(Keys.Enter);
+            //    driver.FindElement(By.XPath(Resources.Submit)).Click();
+
+            IWebElement element = this.driver.FindElement(By.XPath(Resources.SearchResults));
+            Assert.IsTrue(element.Displayed);
+        }
+
+        [NUnit.Framework.TestAttribute()]
+        [NUnit.Framework.CategoryAttribute("Search Engine")]
+        [NUnit.Framework.CategoryAttribute("priority=1")]
+        [NUnit.Framework.Author("anammalu")]
+        public void NegativeTestForBingSearch()
+        {
+            this.driver.Navigate().GoToUrl(Resources.Bing);
+            this.driver.FindElement(By.XPath(Resources.Query)).SendKeys("Extent Reports");
+            this.driver.FindElement(By.XPath(Resources.Query)).SendKeys(Keys.Enter);
+            //    driver.FindElement(By.XPath(Resources.Submit)).Click();
+
+            IWebElement element = this.driver.FindElement(By.XPath(Resources.SearchResults));
+            Assert.IsFalse(element.Displayed);
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            this.driver.Close();
+        }
+    }
+}
---
+++ Reporting/ReportGenerator.cs
@@ -0,0 +1,137 @@
+﻿// <copyright file="ReportGenerator.cs" company="Microsoft">
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+//    THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+//    OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+//    ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+//    OTHER DEALINGS IN THE SOFTWARE.
+// </copyright>
+
+namespace Reporting
+{
+    using System;
+    using System.Collections.Concurrent;
+    using System.Collections.Generic;
+    using System.Collections.Specialized;
+    using System.Configuration;
+    using System.Diagnostics;
+    using System.Linq;
+    using AventStack.ExtentReports;
+    using AventStack.ExtentReports.Reporter;
+    using AventStack.ExtentReports.Reporter.Configuration;
+
+    using NUnit.Framework;
+    using NUnit.Framework.Interfaces;
+
+    /// <summary>
+    /// http://extentreports.com/docs/versions/4/net/
+    /// </summary>
+    internal partial class RandomSearch
+    {        
+        private static readonly NameValueCollection ReportSettings = ConfigurationManager.GetSection("reporting") as NameValueCollection;
+        private static readonly bool Enabled = ReportSettings != null && bool.Parse(ReportSettings["Enabled"]);
+        private static readonly bool ShowSteps = Enabled && bool.Parse(ReportSettings["ShowSteps"]);
+        private static readonly bool DarkTheme = Enabled && bool.Parse(ReportSettings["DarkTheme"]);
+        private static ExtentReports extent;
+        private static ExtentKlovReporter klov;
+        private static ExtentTest extentTest;
+
+
+        [OneTimeSetUp()]
+        public static void ClassInitialize()
+        {
+            try
+            {
+                if (Enabled)
+                {
+                    string style = "body {font-family: 'Segoe UI';}";
+                    ExtentHtmlReporter htmlReporter = new ExtentHtmlReporter(@"E:\POC\Reporting\Reporting\bin\Debug\");
+                    htmlReporter.Config.Theme = DarkTheme ? Theme.Dark : Theme.Standard;
+                    htmlReporter.Config.CSS = style;
+                    extent = new ExtentReports();
+                    extent.AddSystemInfo("Host", Environment.MachineName);
+                    extent.AddSystemInfo("Env", ConfigurationManager.AppSettings["Env"]);
+                    extent.AddSystemInfo("User", Environment.UserName);
+
+                    klov = new ExtentKlovReporter();
+                    klov.InitMongoDbConnection("localhost", 27017);
+                    klov.InitKlovServerConnection("http://localhost:8443");
+                    klov.ProjectName = "Random Search";
+
+                    extent.AttachReporter(klov, htmlReporter);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+
+        [OneTimeTearDown]
+        public static void ClassCleanUp()
+        {
+            try
+            {
+                if (Enabled)
+                {
+                    extent.Flush();
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+
+        [SetUp]
+        public void BeforeTest()
+        {
+            extentTest = extent.CreateTest(TestContext.CurrentContext.Test.ClassName);
+            var categories = TestContext.CurrentContext.Test.Properties["Category"];
+            AddMetadata(extentTest, categories, TestContext.CurrentContext.Test.Properties.Get("Author").ToString());
+        }
+
+        [TearDown]
+        public void AfterTest()
+        {
+            Status logstatus;
+            TestStatus status = TestContext.CurrentContext.Result.Outcome.Status;
+            string stacktrace = string.IsNullOrEmpty(TestContext.CurrentContext.Result.StackTrace)
+                ? ""
+                : string.Format("<pre>{0}</pre>", TestContext.CurrentContext.Result.Message);
+
+            switch (status)
+            {
+                case TestStatus.Failed:
+                    logstatus = Status.Fail;
+                    break;
+                case TestStatus.Inconclusive:
+                    logstatus = Status.Warning;
+                    break;
+                case TestStatus.Skipped:
+                    logstatus = Status.Skip;
+                    break;
+                default:
+                    logstatus = Status.Pass;
+                    break;
+            }
+
+            var node = extentTest.CreateNode(TestContext.CurrentContext.Test.MethodName);
+            node.Log(logstatus, "Test Execution status - " + logstatus + stacktrace);
+        }
+
+        private static void AddMetadata(ExtentTest node, IEnumerable<object> categories, string author)
+        {
+            try
+            {
+                node.AssignCategory(categories.Cast<string>().ToArray());
+                node.AssignAuthor(author);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex);
+            }
+        }
+    }
+}
